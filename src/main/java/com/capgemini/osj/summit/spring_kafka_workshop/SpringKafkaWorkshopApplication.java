7d720e3cc3b9d556611cfdce2897acf9b8f1c4bf
jsquared38 <jpdutoit38@gmail.com>
2025-03-18 15:21:25 +0100

    init

---
+++ src/main/java/com/capgemini/osj/summit/spring_kafka_workshop/SpringKafkaWorkshopApplication.java
@@ -0,0 +1,13 @@
+package com.capgemini.osj.summit.spring_kafka_workshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKafkaWorkshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringKafkaWorkshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/capgemini/osj/summit/spring_kafka_workshop/SpringKafkaWorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capgemini.osj.summit.spring_kafka_workshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringKafkaWorkshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
